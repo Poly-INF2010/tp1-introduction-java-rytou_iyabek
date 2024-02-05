a3ced791fb256c00c255af31bb0a1a0eb49f0951
iyadb <bekriouiiyadd@outlook.fr>
2024-02-05 14:31:09 -0500

    commit
    commit

--- src/main/java/Letter/LetterFactory.java
+++ src/main/java/Letter/LetterFactory.java
@@ -41,7 +41,29 @@ public final class LetterFactory {
      * @return BaseShape containing the letter E
      */
     public static BaseShape create_E() {
-        return null;
+        Rectangle left = new Rectangle(halfStripeThickness - 3,maxHeight);
+        Rectangle up = new Rectangle(halfStripeThickness - 3, halfMaxHeight);
+        Rectangle middle = up.clone();
+        Rectangle down = up.clone();
+
+
+        up.rotate(up.getCoords(), -Math.PI/2);
+        middle.rotate(middle.getCoords(), -Math.PI/2);
+        down.rotate(down.getCoords(), -Math.PI/2);
+
+        left.translate(left.getCoords(), new Point2d(-halfMaxWidth-4, 0.0));
+        up.translate(up.getCoords(), new Point2d(0.0, -halfMaxHeight));
+        down.translate(down.getCoords(), new Point2d(0.0, halfMaxHeight));
+
+
+        left.add(up);
+        left.add(middle);
+        left.add(down);
+
+
+
+
+        return left;
     }
 
     /** TODO
@@ -57,7 +79,14 @@ public final class LetterFactory {
      * @return BaseShape containing the letter N
      */
     public static BaseShape create_N() {
-        return null;
+        Rectangle r1 = new Rectangle(halfStripeThickness, maxHeight);
+        Rectangle r2 = r1.clone();
+        Rectangle r3 = new Rectangle(halfStripeThickness, maxHeight+2);
+        r2.translate(r2.getCoords(), new Point2d(maxWidth, 0.0));
+        r3.rotate(r3.getCoords(), -0.40);
+        r3.translate(r3.getCoords(), new Point2d(maxWidth*0.5, 0.0));
+        r1.add(r2);
+        return r1.add(r3);
     }
 
     /** TODO
@@ -65,7 +94,9 @@ public final class LetterFactory {
      * @return BaseShape containing the letter O
      */
     public static BaseShape create_O() {
-        return null;
+        Ellipse e1 = new Ellipse(maxWidth, maxHeight);
+        Ellipse e2 = new Ellipse(maxWidth-stripeThickness, maxHeight-stripeThickness);
+        e1.remove(e2);
+        return e1;
     }
-
 }
